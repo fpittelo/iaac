fff9ab72f2d8ec31ca74a4ccaca565865628e291
Pitteloud Frederic <frederic.pitteloud@bobst.com>
2020-08-11 08:41:36 +0200

    small update

--- POWERSHELL/wsl.ps1
+++ POWERSHELL/wsl.ps1
@@ -1,3 +1,4 @@
-# install Windows Subsystem for Linux WSL ####################endregion
+# install Windows Subsystem for Linux WSL ####################
+# https://docs.microsoft.com/en-us/windows/wsl/install-win10 #
 
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
