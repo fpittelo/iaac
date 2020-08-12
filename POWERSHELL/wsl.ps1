8667cd7d1b5d2a30b3d8e9af3f12bec77bef3cfe
Pitteloud Frederic <frederic.pitteloud@bobst.com>
2020-08-12 08:38:27 +0200

    comitt

--- POWERSHELL/wsl.ps1
+++
@@ -1,4 +0,0 @@
-# install Windows Subsystem for Linux WSL ####################
-# https://docs.microsoft.com/en-us/windows/wsl/install-win10 #
-
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
